01a2fd363295781cf04c1dfa3c46b6c52bd23244
Your Name <you@example.com>
2024-06-12 13:43:32 -0700

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print('Hello, World')
+    print('CS7: Python Programming')
+
+if __name__ == '__main__':
+    main()
+
+git config --global user.email "shiying.wang3@gmail.com"
+git config --global user.name "Shiying Wang"
